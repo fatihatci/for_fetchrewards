021f82dc61afc4523d6d2ccb04126524c4dc5c0e
fatihatci <78340762+fatihatci@users.noreply.github.com>
2021-02-01 02:25:29 -0500

    Add files via upload

--- text_similarity_final.py
+++ text_similarity_final.py
@@ -1,9 +1,9 @@
 import math, string, sys
 from collections import Counter
 
-t1="The easiest way to earn points with Fetch Rewards is to just shop for the products you already love. If you have any participating brands on your receipt, you'll get points based on the cost of the products. You don't need to clip any coupons or scan individual barcodes. Just scan each grocery receipt after you shop and we'll find the savings for you."
-t2="The easiest way to earn points with Fetch Rewards is to just shop for the items you already buy. If you have any eligible brands on your receipt, you will get points based on the total cost of the products. You do not need to cut out any coupons or scan individual UPCs. Just scan your receipt after you check out and we will find the savings for you."
-t3="We are always looking for opportunities for you to earn more points, which is why we also give you a selection of Special Offers. These Special Offers are opportunities to earn bonus points on top of the regular points you earn every time you purchase a participating brand. No need to pre-select these offers, we'll give you the points whether or not you knew about the offer. We just think it is easier that way."
+text1="The easiest way to earn points with Fetch Rewards is to just shop for the products you already love. If you have any participating brands on your receipt, you'll get points based on the cost of the products. You don't need to clip any coupons or scan individual barcodes. Just scan each grocery receipt after you shop and we'll find the savings for you."
+text2="The easiest way to earn points with Fetch Rewards is to just shop for the items you already buy. If you have any eligible brands on your receipt, you will get points based on the total cost of the products. You do not need to cut out any coupons or scan individual UPCs. Just scan your receipt after you check out and we will find the savings for you."
+text3="We are always looking for opportunities for you to earn more points, which is why we also give you a selection of Special Offers. These Special Offers are opportunities to earn bonus points on top of the regular points you earn every time you purchase a participating brand. No need to pre-select these offers, we'll give you the points whether or not you knew about the offer. We just think it is easier that way."
 
 #expands abbreviated words (i.e. you're -> you are, won't -> will not)
 def expand_abbreviations(text):
@@ -86,13 +86,13 @@ def similarity(text_1, text_2, expand_abbr):
 if __name__ == '__main__':   
 
     # if 3rd argument is set to 1, then abbreviations will be expanded (i.e. you're -> you are, won't -> will not) 
-    similarity(t1, t2, 0)
-    similarity(t1, t2, 1)
+    similarity(text1, text2, 0)
+    similarity(text1, text2, 1)
 
-    similarity(t1, t3, 0)
-    similarity(t1, t3, 1)
+    similarity(text1, text3, 0)
+    similarity(text1, text3, 1)
 
-    similarity(t2, t3, 0)
-    similarity(t2, t3, 1)
+    similarity(text2, text3, 0)
+    similarity(text2, text3, 1)
 
 
